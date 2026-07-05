096d0a73594ed57cdb7cc4db4ba7b236a2af47ea
adeliaeka <143367003+adeliaeka@users.noreply.github.com>
2026-07-05 23:54:13 +0000

    Menghapus nav booking

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ document.getElementById("bookingForm").addEventListener("submit", function(e){
 
 e.preventDefault();
 
-let nama = document.getElementById("nama").value;
-let lapangan = document.getElementById("lapangan").value;
-let tanggal = document.getElementById("tanggal").value;
-let jam = document.getElementById("jam").value;
+let nama=document.getElementById("nama").value;
+let lapangan=document.getElementById("lapangan").value;
+let tanggal=document.getElementById("tanggal").value;
+let jam=document.getElementById("jam").value;
 
-document.getElementById("hasil").innerHTML =
-"Booking berhasil! <br>" +
-"Nama: " + nama + "<br>" +
-"Lapangan: " + lapangan + "<br>" +
-"Tanggal: " + tanggal + "<br>" +
-"Jam: " + jam;
+document.getElementById("hasil").innerHTML=`
+<h2>✅ Booking Berhasil</h2>
+
+<p><b>Nama :</b> ${nama}</p>
+
+<p><b>Lapangan :</b> ${lapangan}</p>
+
+<p><b>Tanggal :</b> ${tanggal}</p>
+
+<p><b>Jam :</b> ${jam}</p>
+
+<p>Terima kasih telah melakukan booking.</p>
+`;
 
 });
