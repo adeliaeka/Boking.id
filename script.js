46971bc0fa340be1f7617aaf05327f27a0f7a879
adeliaeka <143367003+adeliaeka@users.noreply.github.com>
2026-07-06 16:29:42 +0000

    update js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 document.getElementById("bookingForm").addEventListener("submit", function(e){
 
-e.preventDefault();
-
-let nama=document.getElementById("nama").value;
-let lapangan=document.getElementById("lapangan").value;
-let tanggal=document.getElementById("tanggal").value;
-let jam=document.getElementById("jam").value;
-
-document.getElementById("bookingForm").addEventListener("submit", function(e){
-
     e.preventDefault();
 
-    let nama=document.getElementById("nama").value;
-    let lapangan=document.getElementById("lapangan").value;
-    let tanggal=document.getElementById("tanggal").value;
-    let jam=document.getElementById("jam").value;
+    let nama = document.getElementById("nama").value;
+    let lapangan = document.getElementById("lapangan").value;
+    let tanggal = document.getElementById("tanggal").value;
+    let jam = document.getElementById("jam").value;
 
-    document.getElementById("hasil").innerHTML=`
+    document.getElementById("hasil").innerHTML = `
     <h2>✅ Booking Berhasil</h2>
     <p><b>Nama :</b> ${nama}</p>
     <p><b>Lapangan :</b> ${lapangan}</p>
