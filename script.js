eae19a1403c167c77ba91359a6406d7dac67ca9d
adeliaeka <143367003+adeliaeka@users.noreply.github.com>
2026-07-06 16:27:35 +0000

    update

--- script.js
+++ script.js
@@ -7,18 +7,25 @@ let lapangan=document.getElementById("lapangan").value;
 let tanggal=document.getElementById("tanggal").value;
 let jam=document.getElementById("jam").value;
 
-document.getElementById("hasil").innerHTML=`
-<h2>✅ Booking Berhasil</h2>
+document.getElementById("bookingForm").addEventListener("submit", function(e){
 
-<p><b>Nama :</b> ${nama}</p>
+    e.preventDefault();
 
-<p><b>Lapangan :</b> ${lapangan}</p>
+    let nama=document.getElementById("nama").value;
+    let lapangan=document.getElementById("lapangan").value;
+    let tanggal=document.getElementById("tanggal").value;
+    let jam=document.getElementById("jam").value;
 
-<p><b>Tanggal :</b> ${tanggal}</p>
+    document.getElementById("hasil").innerHTML=`
+    <h2>✅ Booking Berhasil</h2>
+    <p><b>Nama :</b> ${nama}</p>
+    <p><b>Lapangan :</b> ${lapangan}</p>
+    <p><b>Tanggal :</b> ${tanggal}</p>
+    <p><b>Jam :</b> ${jam}</p>
+    <p>Terima kasih telah melakukan booking.</p>
+    `;
 
-<p><b>Jam :</b> ${jam}</p>
-
-<p>Terima kasih telah melakukan booking.</p>
-`;
+    document.getElementById("hasil").style.display = "block";
+    document.getElementById("hasil").scrollIntoView({ behavior: "smooth" });
 
 });
