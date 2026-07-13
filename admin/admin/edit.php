7551b5eddff0d32b627ad6f42e35797857427c8e
zidnifaradisa-arch <zidnifaradisa@gmail.com>
2026-07-13 10:47:11 +0000

    update

--- admin/admin/edit.php
+++ admin/admin/edit.php
@@ -11,3 +11,22 @@ $id = $_GET['id'];
 // Ambil Data Booking
 $query = mysqli_query($koneksi, "SELECT * FROM booking WHERE id='$id'");
 $data = mysqli_fetch_assoc($query);
+// Proses Update
+if (isset($_POST['update'])) {
+
+    $nama      = $_POST['nama'];
+    $lapangan  = $_POST['lapangan'];
+    $tanggal   = $_POST['tanggal'];
+    $jam       = $_POST['jam'];
+    $durasi    = $_POST['durasi'];
+    $no_hp     = $_POST['no_hp'];
+
+    $update = mysqli_query($koneksi, "UPDATE booking SET
+        nama='$nama',
+        lapangan='$lapangan',
+        tanggal='$tanggal',
+        jam='$jam',
+        durasi='$durasi',
+        no_hp='$no_hp'
+        WHERE id='$id'
+    ");
