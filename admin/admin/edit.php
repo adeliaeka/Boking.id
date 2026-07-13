c1c6739d8c4b929adba2cb4a86af99e46e2e5196
zidnifaradisa-arch <zidnifaradisa@gmail.com>
2026-07-13 10:46:13 +0000

    update

--- admin/admin/edit.php
+++ admin/admin/edit.php
@@ -5,3 +5,9 @@ $koneksi = mysqli_connect("localhost", "root", "", "sportfield");
 if (!$koneksi) {
     die("Koneksi Gagal : " . mysqli_connect_error());
 }
+// Ambil ID dari URL
+$id = $_GET['id'];
+
+// Ambil Data Booking
+$query = mysqli_query($koneksi, "SELECT * FROM booking WHERE id='$id'");
+$data = mysqli_fetch_assoc($query);
